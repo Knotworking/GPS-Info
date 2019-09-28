e6c1d40fbc267bf9cbf271f214351090715e7de2
Ben Russell <benedictjamesrussell@gmail.com>
2019-09-28 13:00:41 +0200

    [location] basic layout - add more data to UI

--- app/src/main/java/com/knotworking/gpsinfo/ui/main/MainFragment.kt
+++ app/src/main/java/com/knotworking/gpsinfo/ui/main/MainFragment.kt
@@ -1,14 +1,11 @@
 package com.knotworking.gpsinfo.ui.main
 
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.knotworking.gpsinfo.R
 import com.knotworking.gpsinfo.databinding.MainFragmentBinding
@@ -30,29 +27,13 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewModel = injectViewModel(viewModelFactory)
-//        val binding = MainFragmentBinding.inflate(inflater, container, false)
-        val binding: MainFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
-        //TODO check
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+        val binding: MainFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
 
-//        setupUi(binding)
+        binding.lifecycleOwner = this
+        binding.location = viewModel.location
         return binding.root
     }
-
-//    private fun setupUi(binding: MainFragmentBinding) {
-//        //TODO databind directly in layout
-//
-//        val locationObserver = Observer<Location> { location ->
-//            val tempUiString = "lat: ${location.longitude}\n" +
-//                    "long: ${location.latitude}\n" +
-//                    "alt: ${location.altitude}\n"
-//
-//            binding.gpsCoordinates.text = tempUiString
-//        }
-//
-//        viewModel.location.observe(this, locationObserver)
-//    }
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/knotworking/gpsinfo/ui/main/MainViewModel.kt
+++ app/src/main/java/com/knotworking/gpsinfo/ui/main/MainViewModel.kt
@@ -10,7 +10,6 @@ import javax.inject.Inject
  * ViewModel for [MainFragment]
  */
 class MainViewModel @Inject constructor(private val repository: LocationRepository) : ViewModel() {
-//    val longitude = repository.longitude
     val location: LiveData<Location> = repository.location
 
     fun onStart() {
